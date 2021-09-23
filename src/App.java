b4ba5d6b138274ebf2ab01faefe6bfe876789567
Eduardo Peña Acosta <usr1565@salesianos-lacuesta.net>
2021-09-23 22:25:15 +0100

    Se añade las 4 notas al azar, la media de la nota y se imprime en pantalla

--- src/App.java
+++ src/App.java
@@ -3,42 +3,35 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 
-public class main {
+public class App {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-        // Creamos la ArrayList donde almacenaremos los alumnos del archivo .csv
-        ArrayList<String> Alumnos= new ArrayList<String>();
+        /** Creamos la ArrayList donde almacenaremos los alumnos del archivo .csv**/
 
-        //Creamos el Buffer
-        BufferedReader reader = null;
+        ArrayList<String> Alumnos = new ArrayList<String>();
 
-        try{
-
-            //Pedimos leer el archivo csv
-            reader = newBufferedReader( new FileReader("/Users/eduardo/Desktop/2 DAM/PMD/Prácticas/Practica UD 1-1/src/alumnos.csv"));
+        /**Creamos el Buffer**/
 
 
-            //Crearemos una String donde se almacenará provisionalmente lo leído en el archivo.
-            String linea;
-            //Creamos un bucle donde estaremos leyendo el archivo hasta que la línea esté vacía
-            // y a la misma vez los estaremos agregando al ArrayList previamente creado.
-            while((linea=br.readLine())!=null) {
+        /** Introducimos la ruta **/
+
+        String ruta = "/Users/eduardo/Desktop/2 DAM/PMD/Prácticas/Practica UD 1-1/src/alumnos.csv";
+
+        String linea = "";
+
+        try (BufferedReader buffer = new BufferedReader(new FileReader(ruta))) {
+            while ((linea = buffer.readLine()) != null) {
                 Alumnos.add(linea);
-                System.out.println(linea);
             }
-
-        }catch(IOException e){
-            //En caso de error, pues nos saltará.
+        } catch (Exception e) {
             e.printStackTrace();
+
 
         }
 
-
-
     }
-
 }
 

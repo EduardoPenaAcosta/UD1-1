29b727d2cd10af7c8bca21bce50dfdfa2d1ca39b
Eduardo Peña Acosta <usr1565@salesianos-lacuesta.net>
2021-09-28 10:17:19 +0100

    Últimas modificaciones, se han metido ciertas funciones en una clase y optimizado alguna funcion

--- src/App.java
+++ src/App.java
@@ -13,23 +13,23 @@ public class App {
     public static void main(String[] args) {
 
 
-        /** Creamos la ArrayList donde almacenaremos los alumnos del archivo .csv**/
+        // Creamos la ArrayList donde almacenaremos los alumnos del archivo .csv**
         ArrayList<String> Alumnos = new ArrayList<String>();
 
-        /** Creo un array bidimensional para almacenar las notas**/
+        // Creo un array bidimensional para almacenar las notas
         double[] notas = new double[5];
 
-        /** Introducimos la ruta de donde sacaremos los nombres de los alumnos. **/
+        // Introducimos la ruta de donde sacaremos los nombres de los alumnos.
         String ruta = "/Users/eduardo/Desktop/2 DAM/PMD/Prácticas/Practica UD 1-1/src/alumnos.csv";
 
-        /** Utilizaremos esta línea donde almacenaremos temporalmente los nombres de los alumnos**/
+        // Utilizaremos esta línea donde almacenaremos temporalmente los nombres de los alumnos
         String linea = "";
 
-        /** Ahora realizaremos un TryCatch para abrir un buffer,
-         * con este lo que hará será leer y almacenar el archivo,
-         * con el FileReader buscará el archivo.
-         * Luego para almacenarlo, se ha realizado un bucle que mediante el String
-         * de almacenamiento temporal que hemos creado y lo agregamos al ArrayList**/
+        // Ahora realizaremos un TryCatch para abrir un buffer,
+        // con este lo que hará será leer y almacenar el archivo,
+        // con el FileReader buscará el archivo.
+        // Luego para almacenarlo, se ha realizado un bucle que mediante el String
+        // de almacenamiento temporal que hemos creado y lo agregamos al ArrayList
         try (BufferedReader buffer = new BufferedReader(new FileReader(ruta))) {
             while ((linea = buffer.readLine()) != null) {
                 Alumnos.add(linea);
@@ -38,7 +38,7 @@ public class App {
             e.printStackTrace();
         }
 
-        /** Ahora procederemos a crear el archivo donde meteremos los datos. **/
+        // Ahora procederemos a crear el archivo donde meteremos los datos.
         try{
             File newTXT = new File("./notas.txt");
             if(newTXT.createNewFile()){
@@ -54,44 +54,40 @@ public class App {
         FileWriter fichero = null;
         PrintWriter pw = null;
 
-        /** Creo esta variable donde almacenaré todos los alumnos con sus respectivas notas.**/
+        calculoNotas(notas);
+        crearArchivo();
+
+
+        // Creo esta variable donde almacenaré todos los alumnos con sus respectivas notas.
         String alFichero = "";
 
         for(int i = 0; i < 10 ; i++){
 
-            String alumRandom = Alumnos.get((int) (Math.random() * (Alumnos.size() + 0) + 1));
+            String alumRandom = Alumnos.get((int) (Math.random() * (Alumnos.size()) + 1));
 
-            /** Elegimos las 4 notas al azar y realizamos la media de las notas.**/
-            for(int h = 0; h < 4; h++){
-                notas[h] = Math.random() * (10 - 1) + 1;
-                notas[h] = Math.round(notas[h] * 100d)/100d;
 
-            }
 
-            /** Calculamos la media de la nota y redondeamos la nota resultante. **/
-            notas[4] = Math.round( ((notas[0] + notas[1] + notas[2] + notas[3])/4) * 100d)/100d;
-
-            /** Almacenamos el resultado de las notas del alumno e imprimo en pantalla para verificar.**/
+            // Almacenamos el resultado de las notas del alumno e imprimo en pantalla para verificar.
             String frase = "El alumno " + alumRandom + " tiene las siguientes notas: " +  notas[0] + " " +
                     notas[1] + " " + notas[2] + " " + notas[3] + " y la media es de: " + notas[4] + "\n";
 
             System.out.println(frase);
 
-            /** Aquí es donde almacenaré todas las notas del alumno, una seguida de otra.**/
+            // Aquí es donde almacenaré todas las notas del alumno, una seguida de otra.
             alFichero += frase;
 
-            /** Ahora procedemos a escribir el el txt las notas. **/
+            // Ahora procedemos a escribir el el txt las notas.
             try{
-                /** Con el FileWriter nos permite escribir en el archivo **/
-                /** Con el PrintWriter escribimos en el archivo.**/
+                // Con el FileWriter nos permite escribir en el archivo
+                // Con el PrintWriter escribimos en el archivo.
                 fichero = new FileWriter("./src/notas.txt");
                 pw = new PrintWriter(fichero);
 
-                    /** Aquí escribiremos la frase en el txt.**/
+                   // Aquí escribiremos la frase en el txt.
                     fichero.write(alFichero);
 
-            /** En caso de errores, pues saltará el error, en case de que
-             * no, se cerrará el FileWriter.**/
+            // En caso de errores, pues saltará el error, en case de que
+                // no, se cerrará el FileWriter.
             }catch(Exception e){
                 e.printStackTrace();
             } finally {
@@ -105,4 +101,36 @@ public class App {
             }
         }
     }
+
+    public static void crearArchivo(){
+
+        // Ahora procederemos a crear el archivo donde meteremos los datos.
+        try{
+            File newTXT = new File("./notas.txt");
+            if(newTXT.createNewFile()){
+                System.out.println("El txt ha sido creado correctamente, ahora se procederá a añadir las notas.");
+
+            }else{
+                System.out.println("El archivo ya existe, se modificará al existente...");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void calculoNotas(double[] notas){
+        // Elegimos las 4 notas al azar y realizamos la media de las notas.**
+        for(int h = 0; h < 4; h++){
+            notas[h] = Math.random() * (10 - 1) + 1;
+            notas[h] = Math.round(notas[h] * 100d)/100d;
+
+        }
+
+        //Calculamos la media de la nota y redondeamos la nota resultante.
+        notas[4] = Math.round( ((notas[0] + notas[1] + notas[2] + notas[3])/4) * 100d)/100d;
+    }
+
+
+
 }
